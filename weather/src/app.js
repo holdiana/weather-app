453f0509c35e80e97e925a6f48ba0b5bf8a34e4f
Diana Holoborodko <diana.holoborodko@gmail.com>
2022-07-06 15:21:22 +0200

    Update the time

--- weather/src/app.js
+++ weather/src/app.js
@@ -1,3 +1,27 @@
+function formateDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tyesday",
+    "Wednesday",
+    "Thursday",
+    "Freiday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -6,11 +30,14 @@ function displayTemperature(response) {
   let precipitationElement = document.querySelector("#precipitation");
   let speedWind = document.querySelector("#wind");
   let humidityElement = document.querySelector("#humidity");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityName.innerHTML = response.data.name;
   description.innerHTML = response.data.weather[0].description;
   speedWind.innerHTML = Math.round(response.data.wind.speed);
   humidityElement.innerHTML = response.data.main.humidity;
+  //використання функції формату дня та часу
+  dateElement.innerHTML = formateDate(response.data.dt * 1000);
 }
 
 let apiKey = "f48cb8e5ea750137e496c7dd7e72c4af";
